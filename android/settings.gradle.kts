812595f59cc7e1078788590c0eda55dfbc898e41
khanhlinh1111 <khanhlinhak12@gmail.com>
2025-12-02 17:33:36 +0700

    fix: Downgrade AGP to 7.3.0 and Gradle to 7.5 to fix Isar build issue

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,29 +19,8 @@ pluginManagement {
 
 plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.11.1" apply false
-    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
+    id("com.android.application") version "7.3.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.7.10" apply false
 }
 
 include(":app")
-
-// Workaround for libraries not specifying namespace (AGP 8+)
-gradle.lifecycle.beforeProject {
-    if (file("src/main/AndroidManifest.xml").exists()) {
-        try {
-            val manifest = file("src/main/AndroidManifest.xml").readText()
-            val packageNameRegex = "package=\"([^\"]+)\"".toRegex()
-            val packageName = packageNameRegex.find(manifest)?.groupValues?.get(1)
-
-            if (packageName != null) {
-                extensions.configure<com.android.build.gradle.BaseExtension> {
-                    if (namespace == null) {
-                        namespace = packageName
-                    }
-                }
-            }
-        } catch (e: Exception) {
-            // Ignore errors if extension configuration fails
-        }
-    }
-}
