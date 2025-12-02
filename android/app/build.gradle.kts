85603a9b4c59b8312ab8092a55e86e0b6af46c97
khanhlinh1111 <khanhlinhak12@gmail.com>
2025-12-02 17:54:56 +0700

    fix: Force androidx.activity version to 1.9.3 to avoid AGP requirements

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,7 +1,6 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
@@ -20,10 +19,7 @@ android {
     }
 
     defaultConfig {
-        // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
         applicationId = "com.example.cobay_manager"
-        // You can update the following values to match your application needs.
-        // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
@@ -32,8 +28,6 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
         }
     }
@@ -41,4 +35,12 @@ android {
 
 flutter {
     source = "../.."
+}
+
+// === FIX: FORCE ANDROIDX ACTIVITY VERSION ===
+// Ép dùng bản ổn định để tránh lỗi đòi hỏi AGP 8.9.1
+configurations.all {
+    resolutionStrategy {
+        force("androidx.activity:activity:1.9.3")
+    }
 }
