01ae714d107847dbb9148b400aed1093b92ffb24
khanhlinh1111 <khanhlinhak12@gmail.com>
2025-12-02 17:37:54 +0700

    fix: Upgrade to AGP 8.1 + Gradle 8.1 and add namespace auto-fix script

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -22,3 +22,19 @@ subprojects {
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
+
+// WORKAROUND: Fix lỗi thiếu namespace ở thư viện cũ (như isar_flutter_libs) khi dùng AGP 8
+subprojects {
+    afterEvaluate {
+        // Kiểm tra xem project con này có phải là Android Library không
+        if (project.plugins.hasPlugin("com.android.library")) {
+            val android = project.extensions.getByName("android") as com.android.build.gradle.LibraryExtension
+            // Nếu chưa có namespace thì tự tạo
+            if (android.namespace == null) {
+                val defaultNamespace = "com.example.${project.name.replace("-", "_")}"
+                println("AUTO-FIX: Setting namespace for ${project.name} to $defaultNamespace")
+                android.namespace = defaultNamespace
+            }
+        }
+    }
+}
